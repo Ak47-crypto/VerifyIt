aae9064fa70529737c03d8d11084caa66e95fd58
Ankur Kumar <56749949+Ak47-crypto@users.noreply.github.com>
2023-12-03 16:57:23 +0530

    Update SiteStates.jsx host

--- frontend/src/context/SiteStates.jsx
+++ frontend/src/context/SiteStates.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import SiteContext from './SiteContext'
 function SiteStates(Props) {
-  const host='http://localhost:3000'
+  const host='https://verifyit-backend.vercel.app'
   // https://verifyit-backend.vercel.app
   const [alerts,setAlerts]=useState(null);
   const [connection, setConnection] = useState(false)
