92a953cc93e6f92c0a3bfe4af1709e0747836a98
Ankur Kumar <ak6550352@gmail.com>
2023-12-03 22:08:14 +0530

    cheching localhost cors

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const secreatKey='akisdon'
 connectToMongo();
 app.use(cors(
      {
-         origin: ["https://verifyit.netlify.app","https://verifyit.vercel.app"],
+         origin: ["https://verifyit.netlify.app","https://verifyit.vercel.app","http://localhost:3000"],
          methods: ["POST", "GET","DELETE","PUT"], 
          credentials: true
      }
